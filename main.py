5682e45edb5a24649127c429a854b6be237ff11c
github_saranyab21 <saranyab1912@gmail.com>
2021-09-19 20:26:18 +0530

    Updated code

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def main():
     dp.add_handler(MessageHandler(Filters.text, handle_message))
 
     updater.start_webhook( listen = "0.0.0.0", port = port, url_path = API_KEY)
-    updater.bot.setWebhook("https://pacific-bastion-45498.herokuapp.com/" + API_KEY)
+    updater.bot.setWebhook("https://agile-mountain-59201.herokuapp.com/" + API_KEY)
     updater.idle()
 
 
